7331900b670ade09d24f1abc38f53c276b3b8f6d
aramirez <tonifer627@gmail.com>
2017-08-01 16:25:40 -0400

    commit  hola en rama1

--- archivos/login.java
+++ archivos/login.java
@@ -1,7 +1,12 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ */
+
+
+/*
+ * hola
  */
 package Controler;
 
